46e569834b4a52732cb5927b8a0d290b9a15781c
rawatsajayGIT <124587460+rawatsajayGIT@users.noreply.github.com>
2023-02-06 16:23:09 +0000

    Update streamlit_app.py
    added breakfast menu and 3 item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,5 @@ streamlit.title('My Parents New Healty Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
