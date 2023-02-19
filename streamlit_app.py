8c30e70e5cc44291f4e38062e8a26cef3e5978c0
rawatsajayGIT <124587460+rawatsajayGIT@users.noreply.github.com>
2023-02-19 16:03:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?)
-streamlit.write('Thanks for adding ', add_my_fruit)
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
